a23467763683932286e4b7736a13dd9babc413bf
James Noble III <jimnoble@gmail.com>
2013-11-19 18:13:26 -0800

    sparrow alpha 1

---
+++ gruntfile.js
@@ -0,0 +1,25 @@
+﻿
+module.exports = function (grunt)
+{
+    grunt.initConfig(
+    {
+        browserify:
+        {
+            client:
+            {
+                src: ['./lib/client.js'],
+
+                dest: './lib/content/client.js',
+
+                options:
+                {
+                    alias: ['./lib/client:client']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+
+    grunt.registerTask('default', ['browserify']);
+};
